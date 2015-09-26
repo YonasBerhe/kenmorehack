cfc97d0befda09218235b95b8626c3a168b84409
Louis Vichy <lgvichy@gmail.com>
2015-09-26 12:35:11 -0700

    added landing page, background, and routing

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,5 +1,35 @@
 angular.module('starter.controllers', [])
+.controller(function($scope, $ionicAnimation) {
+   var anim = $ionicAnimation({
+    // A unique, reusable name
+    name: 'popIn',
 
+    // The duration of an auto playthrough
+    duration: 0.5,
+
+    // How long to wait before running the animation
+    delay: 0,
+
+    // Whether to reverse after doing one run through
+    autoReverse: false,
+
+    // How many times to repeat? -1 or null for infinite
+    repeat: -1,
+
+    // Timing curve to use (same as CSS timing functions), or a function of time "t" to handle it yourself
+    curve: 'ease-in-out',
+
+    onStart: function() {
+      // Callback on start
+    },
+    onEnd: function() {
+      // Callback on end
+    },
+    step: function(amt) {
+
+    }
+  });
+})
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
